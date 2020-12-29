66f6e14c88178f229f007576cfdac415bdf187ab
lefosfk <eleniskg@mail.com>
2020-12-28 22:55:56 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Elena says, Hello World!'));
+app.get('/', (req, res) => res.send('Stupid says, Hello World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
